94857cea46a1378983dab21c3eb757606ff3229e
aryann501 <aryanmalhotra559@gmail.com>
2026-02-11 08:21:44 +0000

    Log files made

--- backend/src/main/java/com/example/backend/exceptions/GlobalExceptionHandler.java
+++ backend/src/main/java/com/example/backend/exceptions/GlobalExceptionHandler.java
@@ -1,34 +1,44 @@
 package com.example.backend.exceptions;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
     @ExceptionHandler(AccountNotFoundException.class)
     public ResponseEntity<String> handleAccountNotFound(AccountNotFoundException ex) {
+        logger.error("{}",ex.getMessage());
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(AccountNotActiveException.class)
     public ResponseEntity<String> handleAccountNotActive(AccountNotActiveException ex) {
+        logger.error("{}",ex.getMessage());
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(InsufficientBalanceException.class)
     public ResponseEntity<String> handleInsufficientBalance(InsufficientBalanceException ex) {
+        logger.error("{}",ex.getMessage());
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(DuplicateTransferException.class)
     public ResponseEntity<String> handleDuplicateTransfer(DuplicateTransferException ex) {
+        logger.error("{}",ex.getMessage());
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
     }
     @ExceptionHandler(UserAuthenticationException.class)
     public ResponseEntity<String> handleUserAuthentication(UserAuthenticationException ex) {
+        logger.error("{}",ex.getMessage());
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
     }
 }
